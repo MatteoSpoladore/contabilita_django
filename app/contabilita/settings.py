4582695901871b266c114b7355090d13db173e6c
Matteo <matteo.spoladore99@gmail.com>
2025-10-19 14:43:16 +0200

    creazione gestione_utenti

--- app/contabilita/settings.py
+++ app/contabilita/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "anagrafica",
+    "gestione_utenti",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
